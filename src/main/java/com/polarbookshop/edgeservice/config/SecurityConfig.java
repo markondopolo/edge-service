964766dea4d89db027d053dc73588c1ce87b954d
Marko Korovin <markondopolo@gmail.com>
2025-07-18 15:33:25 +0200

    add basic OpenID connect authentication & get user info from ID token

--- src/main/java/com/polarbookshop/edgeservice/config/SecurityConfig.java
+++ src/main/java/com/polarbookshop/edgeservice/config/SecurityConfig.java
@@ -2,7 +2,6 @@ package com.polarbookshop.edgeservice.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
@@ -14,7 +13,7 @@ public class SecurityConfig {
     SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
     return http
             .authorizeExchange(exchange -> exchange.anyExchange().authenticated())
-            .formLogin(Customizer.withDefaults())
+            .oauth2Login(Customizer.withDefaults())
             .build();
     }
 }
